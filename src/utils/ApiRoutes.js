140ae68bd629e1b8376df357b958553a1cde7677
dulabdul <dul.abdoel21@gmail.com>
2022-10-23 04:46:12 +0700

    remove slash

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'https://chatty-serverr.herokuapp.com/';
+export const host = 'https://chatty-serverr.herokuapp.com';
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const avatarRoute = `${host}/api/auth/avatar`;
